67027d3ba2c37762104ea010915299969450260a
Raiquen <raiqueng@live.com>
2025-04-07 18:17:45 +0200

    chore: make zip script

---
+++ scripts/make-zip-folder.js
@@ -0,0 +1,39 @@
+//@ts-ignore
+import JSZip from 'jszip'
+import fs from 'fs'
+import path from 'path'
+
+async function addFile(zip, filePath) {
+    const data = fs.readFileSync(filePath)
+    const fileName = path.basename(filePath)
+    zip.file(fileName, data)
+}
+
+async function addFolder(zip, folderPath) {
+    const files = fs.readdirSync(folderPath)
+    const folderName = path.basename(folderPath)
+    const folder = zip.folder(folderName)
+    for (const file of files) {
+        const filePath = path.join(folderPath, file)
+        if (fs.statSync(filePath).isDirectory()) {
+            await addFolder(folder, filePath)
+        } else {
+            await addFile(folder, filePath)
+        }
+    }
+}
+
+export async function createZip(files) {
+    const zip = new JSZip()
+
+    for (const file of files) {
+        await addFile(zip, file)
+    }
+
+    zip.generateAsync({ type: 'nodebuffer' }).then(content => {
+        fs.writeFileSync('dist/one-click-block-twitter.zip', content)
+    })
+}
+
+export const filesToAdd = ['manifest.json', 'src/content.js']
+
